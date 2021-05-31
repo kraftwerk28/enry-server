8bef4fdf1c50c19a915b3bcd49b0906df2c08aa8
kraftwerk28 <kefirchik3@gmail.com>
2021-05-31 23:05:27 +0300

    chore

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module enry-server
 
 go 1.16
 
-require github.com/go-enry/go-enry/v2 v2.7.0 // indirect
+require (
+	github.com/go-enry/go-enry/v2 v2.7.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,49 +10,126 @@ import (
 	"strings"
 
 	"github.com/go-enry/go-enry/v2"
+	"github.com/julienschmidt/httprouter"
 )
 
-var availableLanguages []string = []string{
-	"Python",
-	"Haskell",
-	"Javascript",
-	"Typescript",
-	"C++",
-	"C",
-	"Go",
-	"Lua",
-	"Rust",
+type LanguageCfg struct {
+	name          string
+	commentstring string
 }
 
-func handleIndex(w http.ResponseWriter, req *http.Request) {
-	languages := req.URL.Query()["languages"]
-	if len(languages) == 0 {
-		languages = availableLanguages
-	} else {
-		languages = strings.Split(languages[0], ",")
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type DetectedLangResponse struct {
+	Language      string `json:"language"`
+	Extension     string `json:"extension"`
+	Commentstring string `json:"commentstring"`
+}
+
+var availableLanguages = []LanguageCfg{
+	{"Python", "# %s"},
+	{"Haskell", "-- %s"},
+	{"Javascript", "// %s"},
+	{"Typescript", "// %s"},
+	{"C++", "// %s"},
+	{"C", "// %s"},
+	{"Go", "// %s"},
+	{"Lua", "-- %s"},
+	{"Rust", "// %s"},
+}
+
+func normalizeLangList() {
+	for i, cfg := range availableLanguages {
+		oldName := cfg.name
+		lang, ok := enry.GetLanguageByAlias(oldName)
+		if !ok {
+			log.Fatalf(
+				"\"%s\" is no a valid language name among `availableLanguages`",
+				oldName,
+			)
+		}
+		availableLanguages[i].name = lang
 	}
-	if len(languages) == 0 {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Empty language list is not allowed"))
+}
+
+func getCommentString(lang string) string {
+	for _, cfg := range availableLanguages {
+		if cfg.name == lang {
+			return cfg.commentstring
+		}
+	}
+	return ""
+}
+
+// We assume, that Content-Type is already application/json
+func fail(res http.ResponseWriter, code int, message string) {
+	res.WriteHeader(code)
+	body, _ := json.Marshal(ErrorResponse{message})
+	res.Write(body)
+}
+
+func handleDetectLanguage(
+	w http.ResponseWriter,
+	req *http.Request,
+	ps httprouter.Params,
+) {
+	w.Header().Add("Content-Type", "application/json")
+	rawLanguagesFromQuery := req.URL.Query().Get("languages")
+	var candidates []string
+
+	if rawLanguagesFromQuery == "" {
+		candidates = make([]string, len(availableLanguages))
+		for i := range availableLanguages {
+			candidates[i] = availableLanguages[i].name
+		}
+	} else {
+		candidates = strings.Split(rawLanguagesFromQuery, ",")
+	}
+
+	if len(candidates) == 0 {
+		fail(w, http.StatusBadRequest, "Empty language list is not allowed")
 		return
 	}
+
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		fail(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	lang := enry.GetLanguagesByClassifier("", body, languages)
-	result := strings.ToLower(lang[0])
-	w.Write([]byte(result))
+
+	lang, _ := enry.GetLanguageByClassifier(body, candidates)
+	if lang == "" {
+		fail(w, http.StatusNotFound, "Langauge not detected")
+		return
+	}
+
+	// Ok will always be true
+	ext := enry.GetLanguageExtensions(lang)
+	commentString := getCommentString(lang)
+	resBody, _ := json.Marshal(DetectedLangResponse{
+		lang,
+		ext[0][1:],
+		commentString,
+	})
+	w.WriteHeader(http.StatusOK)
+	w.Write(resBody)
 }
 
-func main() {
-	http.HandleFunc("/", handleIndex)
+func setupServer() {
+	router := httprouter.New()
+	router.POST("/", handleDetectLanguage)
 	port := (os.Getenv("PORT"))
 	if port == "" {
 		port = "8080"
 	}
 	log.Printf("Server running on :%s\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil))
+	err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), router)
+	log.Fatal(err)
+}
+
+func main() {
+	normalizeLangList()
+	setupServer()
 }
